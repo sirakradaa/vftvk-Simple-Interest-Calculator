86ad246ae806ae6f344f173ecc8faa9a6b718bc0
Sirak Radaa <radaa.sirak@gmail.com>
2021-09-27 20:59:47 -0600

    Principal test for <= 0

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ function compute() {
     var years = document.getElementById("years").value;
     var interest = principal * years * rate / 100;
     var year = new Date().getFullYear() + parseInt(years);
+
     document.getElementById("result").innerHTML = "If you deposit <mark>" + principal + "</mark>,\<br\>at an interest rate of <mark>" + rate + "%</mark>\<br\>You will receive an amount of <mark>" + interest + "</mark>,\<br\>in the year <mark>" + year + "</mark>\<br\>"
 
 
@@ -18,9 +19,9 @@ function updateRate() {
 
 function principalCheck() {
     var principal = document.getElementById("principal").value;
-    var greaterThan = parseInt(principal) > 0;
-    if (!greaterThan) {
+    if (principal <= 0) {
         alert("Enter a positive number");
         document.getElementById("principal").focus();
+        return false
     }
 }
