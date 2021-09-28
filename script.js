afbfb07ad716bfef2aba953af81151e13309c190
Sirak Radaa <radaa.sirak@gmail.com>
2021-09-27 22:38:24 -0600

    Added comments to js, need to fix title (html) and alignment (css)

--- script.js
+++ script.js
@@ -6,12 +6,14 @@ function compute() {
     var interest = principal * years * rate / 100;
     var year = new Date().getFullYear() + parseInt(years);
 
+    //Check principal amount
     if (principal <= 0) {
         alert("Enter a positive number");
         document.getElementById("principal").focus();
         return false
     }
 
+    //Print result
     document.getElementById("result").innerHTML = "If you deposit <mark>" + principal + "</mark>,\<br\>at an interest rate of <mark>" + rate + "%</mark>\<br\>You will receive an amount of <mark>" + interest + "</mark>,\<br\>in the year <mark>" + year + "</mark>\<br\>"
 
 
@@ -21,13 +23,4 @@ function compute() {
 function updateRate() {
     var rateval = document.getElementById("rate").value;
     document.getElementById("rate_val").innerText = rateval;
-}
-
-function principalCheck() {
-    var principal = document.getElementById("principal").value;
-    if (principal <= 0) {
-        alert("Enter a positive number");
-        document.getElementById("principal").focus();
-        return false
-    }
 }
