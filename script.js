d0889c222e5b2c19e2d0f22823b2bfd0b04a0871
Sirak Radaa <radaa.sirak@gmail.com>
2021-09-27 21:01:04 -0600

    Principal test in compute function

--- script.js
+++ script.js
@@ -6,6 +6,12 @@ function compute() {
     var interest = principal * years * rate / 100;
     var year = new Date().getFullYear() + parseInt(years);
 
+    if (principal <= 0) {
+        alert("Enter a positive number");
+        document.getElementById("principal").focus();
+        return false
+    }
+
     document.getElementById("result").innerHTML = "If you deposit <mark>" + principal + "</mark>,\<br\>at an interest rate of <mark>" + rate + "%</mark>\<br\>You will receive an amount of <mark>" + interest + "</mark>,\<br\>in the year <mark>" + year + "</mark>\<br\>"
 
 
